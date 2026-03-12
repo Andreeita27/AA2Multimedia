60c0d5f6489664dbc05954bff411ff42427962ed
Andrea Fernandez de la Rosa <a28707@svalero.com>
2026-03-12 16:46:52 +0100

    El personaje salta y se mueve

--- core/src/main/java/com/svalero/com/screen/GameScreen.java
+++ core/src/main/java/com/svalero/com/screen/GameScreen.java
@@ -1,45 +1,128 @@
 package com.svalero.com.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.svalero.com.util.Constants;
 
-/** First screen of the application. Displayed after the application is created. */
 public class GameScreen implements Screen {
+
+    private SpriteBatch batch;
+
+    private Texture background;
+    private Texture player;
+    private Texture ground;
+
+    private Vector2 playerPosition;
+    private Vector2 playerVelocity;
+
+    private boolean onGround;
+
     @Override
     public void show() {
-        // Prepare your screen here.
+        batch = new SpriteBatch();
+
+        background = new Texture(Gdx.files.internal("grassbackground.png"));
+        player = new Texture(Gdx.files.internal("player.png"));
+        ground = new Texture(Gdx.files.internal("grassground.png"));
+
+        playerPosition = new Vector2(100, Constants.GROUND_Y);
+        playerVelocity = new Vector2(0, 0);
+
+        onGround = true;
     }
 
     @Override
     public void render(float delta) {
-        // Draw your screen here. "delta" is the time since last render in seconds.
+        handleInput(delta);
+        applyGravity(delta);
+        updatePlayer(delta);
+
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        // suelo simple repetido
+        for (int x = 0; x < Gdx.graphics.getWidth(); x += 64) {
+            batch.draw(ground, x, 0, 64, 80);
+        }
+
+        batch.draw(player, playerPosition.x, playerPosition.y, 64, 64);
+
+        batch.end();
+    }
+
+    private void handleInput(float delta) {
+        playerVelocity.x = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            playerVelocity.x = -Constants.PLAYER_SPEED;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            playerVelocity.x = Constants.PLAYER_SPEED;
+        }
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && onGround) {
+            playerVelocity.y = Constants.JUMP_FORCE;
+            onGround = false;
+        }
+    }
+
+    private void applyGravity(float delta) {
+        if (!onGround) {
+            playerVelocity.y -= Constants.GRAVITY * delta;
+        }
+    }
+
+    private void updatePlayer(float delta) {
+        playerPosition.x += playerVelocity.x * delta;
+        playerPosition.y += playerVelocity.y * delta;
+
+        if (playerPosition.y <= Constants.GROUND_Y) {
+            playerPosition.y = Constants.GROUND_Y;
+            playerVelocity.y = 0;
+            onGround = true;
+        }
+
+        if (playerPosition.x < 0) {
+            playerPosition.x = 0;
+        }
+
+        float maxX = Gdx.graphics.getWidth() - 64;
+        if (playerPosition.x > maxX) {
+            playerPosition.x = maxX;
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-        // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
-        // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
-        if(width <= 0 || height <= 0) return;
-
-        // Resize your screen here. The parameters represent the new window size.
     }
 
     @Override
     public void pause() {
-        // Invoked when your application is paused.
     }
 
     @Override
     public void resume() {
-        // Invoked when your application is resumed after pause.
     }
 
     @Override
     public void hide() {
-        // This method is called when another screen replaces this one.
     }
 
     @Override
     public void dispose() {
-        // Destroy screen's assets here.
+        batch.dispose();
+        background.dispose();
+        player.dispose();
+        ground.dispose();
     }
 }
---
+++ core/src/main/java/com/svalero/com/util/Constants.java
@@ -0,0 +1,10 @@
+package com.svalero.com.util;
+
+public class Constants {
+
+    public static final float PLAYER_SPEED = 220f;
+    public static final float JUMP_FORCE = 500f;
+    public static final float GRAVITY = 900f;
+
+    public static final float GROUND_Y = 80f;
+}
