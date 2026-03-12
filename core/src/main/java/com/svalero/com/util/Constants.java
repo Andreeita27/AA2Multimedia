b5e50afe8c3e065237f5a36ed73559f48089ac9d
Andrea Fernandez de la Rosa <a28707@svalero.com>
2026-03-12 17:30:07 +0100

    Añado a la rana enemiga

--- core/src/main/java/com/svalero/com/util/Constants.java
+++ core/src/main/java/com/svalero/com/util/Constants.java
@@ -15,4 +15,5 @@ public class Constants {
     public static final float WORLD_HEIGHT = 480f;
 
     public static final int POINTS_PER_CRYSTAL = 10;
+    public static final int INITIAL_LIVES = 3;
 }
