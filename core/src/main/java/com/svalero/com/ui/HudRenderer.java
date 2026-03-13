a0e4e7b842ad44b08af331c20de280dfa699f6ef
Andrea Fernandez de la Rosa <a28707@svalero.com>
2026-03-13 17:18:20 +0100

    Modifico el hud

---
+++ core/src/main/java/com/svalero/com/ui/HudRenderer.java
@@ -0,0 +1,89 @@
+package com.svalero.com.ui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class HudRenderer {
+
+    private final BitmapFont font;
+    private final BitmapFont smallFont;
+    private final Texture heartIcon;
+    private final Texture gemIcon;
+    private final Texture panelTexture;
+
+    public HudRenderer() {
+        font = new BitmapFont();
+        font.getData().setScale(1.15f);
+        font.setColor(Color.WHITE);
+
+        smallFont = new BitmapFont();
+        smallFont.getData().setScale(0.95f);
+        smallFont.setColor(new Color(0.92f, 0.92f, 0.92f, 1f));
+
+        heartIcon = new Texture("hearthud.png");
+        gemIcon = new Texture("gemhud.png");
+
+        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(0f, 0f, 0f, 0.45f);
+        pixmap.fill();
+        panelTexture = new Texture(pixmap);
+        pixmap.dispose();
+    }
+
+    public void draw(
+        SpriteBatch batch,
+        float cameraLeft,
+        float screenHeight,
+        int lives,
+        int maxLives,
+        int collectedGems,
+        int totalGems,
+        int score,
+        int level
+    ) {
+        float panelX = cameraLeft + 14;
+        float panelY = screenHeight - 108;
+        float panelWidth = 255;
+        float panelHeight = 92;
+
+        batch.draw(panelTexture, panelX, panelY, panelWidth, panelHeight);
+
+        float topRowY = screenHeight - 28;
+        float bottomRowY = screenHeight - 64;
+
+        // Corazón
+        batch.draw(heartIcon, panelX + 12, topRowY - 20, 20, 20);
+        drawTextWithShadow(batch, font, lives + "/" + maxLives, panelX + 40, topRowY);
+
+        // Gema
+        batch.draw(gemIcon, panelX + 115, topRowY - 20, 20, 20);
+        drawTextWithShadow(batch, font, collectedGems + "/" + totalGems, panelX + 145, topRowY);
+
+        // Puntos
+        drawTextWithShadow(batch, smallFont, "Puntos: " + score, panelX + 14, bottomRowY);
+
+        // Nivel
+        drawTextWithShadow(batch, smallFont, "Nivel " + level, panelX + 150, bottomRowY);
+    }
+
+    private void drawTextWithShadow(SpriteBatch batch, BitmapFont font, String text, float x, float y) {
+        Color oldColor = font.getColor().cpy();
+
+        font.setColor(0f, 0f, 0f, 0.7f);
+        font.draw(batch, text, x + 1.5f, y - 1.5f);
+
+        font.setColor(oldColor);
+        font.draw(batch, text, x, y);
+    }
+
+    public void dispose() {
+        font.dispose();
+        smallFont.dispose();
+        heartIcon.dispose();
+        gemIcon.dispose();
+        panelTexture.dispose();
+    }
+}
